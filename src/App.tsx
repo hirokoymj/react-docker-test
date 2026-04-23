016cf787f92d604fd0f2d5ecce93040f5ec12ffa
hirokoymj <hiroko@hirokoymj.com>
2026-04-23 12:23:09 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>React Test app for Docker </h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
